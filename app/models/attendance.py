a83e205bacaceb5601708cb76c05b47b7af21f93
Yash Jogi <jogiyash01@gmail.com>
2025-08-08 00:38:22 +0530

    updated attendance model, added relationship with user table and updated timezone to true for timein and timeout

--- app/models/attendance.py
+++ app/models/attendance.py
@@ -3,12 +3,16 @@ from sqlalchemy.orm import relationship
 from app.models.base_model import BaseModel
 from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
+import pytz
+
+ist = pytz.timezone('Asia/Kolkata')
 
 class Attendance(BaseModel):
     __tablename__ = "attendances"
     user_id = Column(UUID, ForeignKey("users.id"))
-    date = Column(Date, default=datetime.today())
-    time_in = Column(DateTime)
-    time_out = Column(DateTime)
+    date = Column(Date, default=datetime.now(ist).date())
+    time_in = Column(DateTime(timezone=True), default=datetime.now(ist))
+    time_out = Column(DateTime(timezone=True), nullable=True)
     created_by = Column(UUID, ForeignKey("users.id", ondelete="CASCADE"))
+    user = relationship("User", foreign_keys="attendances.user_id", back_populates="attendances", uselist=False)
     
